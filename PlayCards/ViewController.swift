123cf4941a70da823acde8e4b3f8180fc8b16938
Sergey Borisov <it.saborisov@gmail.com>
2020-09-09 23:00:20 +0300

    addFirstCommit

--- PlayCards/ViewController.swift
+++ PlayCards/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var touches = 0 {
+        //абсерверы - наблюдатели
+        didSet{
+            touchesLabel.text = "Попыток: \(touches)"
+        }
+    }
+    
+    
+    let emojiCollection = ["1","🐷","🦁","🐷","1","🦁"]
+    
+    
+    func flipCard(emodji:String, button:UIButton)  {
+        if button.currentTitle == emodji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+            
+        }else {
+            button.setTitle(emodji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    @IBOutlet weak var touchesLabel: UILabel!
+    
+    @IBAction func cardButtonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender){
+            flipCard(emodji: emojiCollection[buttonIndex], button: sender)
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
